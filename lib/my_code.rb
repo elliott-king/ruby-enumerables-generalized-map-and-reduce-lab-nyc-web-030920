08ae5e377be72f45c6fc5a8f906fda62f754b704
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-26 22:42:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,12 @@ def map source
   end
   return ret
 end
+
+def reduce source start=0
+  # either start is index or starting value?
+  ret = start
+  source.each do |val|
+    ret += (yield val)
+  end
+  return ret
+end
